ddf3b33278bdf55b61c3684fa2998e05555555c4
Gokul S <iamsgokul@gmail.com>
2025-09-28 21:35:00 +0530

    Add files via upload
    update js

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
-$(document).ready(function () {
-    $('.mobmenu').click(function () {
-        $('.menu ul').toggleClass('active');
-    })
-})
+document.addEventListener("DOMContentLoaded", function () {
+  const mobmenu = document.querySelector(".mobmenu");
+  if (mobmenu) {
+    mobmenu.addEventListener("click", function () {
+      const menuUl = document.querySelector(".menu ul");
+      if (menuUl) {
+        menuUl.classList.toggle("active");
+      }
+    });
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("headerbar").innerHTML = data;
+    });
+
+  fetch("footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footerbar").innerHTML = data;
+    });
+
+  fetch("whatsapp.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("whatsapp").innerHTML = data;
+    });
+});
